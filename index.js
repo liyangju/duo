633cc9ed3d180befc672f118eac2a16986b68e8b
lige <32735332+liyangju@users.noreply.github.com>
2023-05-27 00:18:43 +0800

    Update index.js

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.get('/', async (req, res) => {
 
   if (userAgent === 'okhttp/3.15' || userAgent === 'okhttp/5.0.0-alpha.10' || userAgent === 'okhttp/3.12.10' || userAgent === 'http/3.12.11' || userAgent === 'okhttp/3.12.13' || userAgent === 'okhttp/4.10.0' || userAgent === 'okhttp/4.11.0') {
     try {
-      const { data } = await axios.get('/y/xm.json');
+      const { data } = await axios.get('https://duo-lige.vercel.app/y/xm.json');
       res.send(data);
     } catch (error) {
       console.error(error);
